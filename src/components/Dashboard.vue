a20b771ea57143dd429df5cb6583170815fc0fb9
DarrenL <darrenlimweiyang@gmail.com>
2020-10-21 12:00:31 +0800

    Added welcome back continer and AY for dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,5 +1,20 @@
 <template>
-    <h1> Dashboard </h1>
+    <div class="app">
+    <div id="container">
+        <div id="first">
+            <p id='personal_message'> Welcome back, John Doe </p>
+
+        </div>
+        <div id="second">
+            <p id='school_term'> 
+                <br>
+                AY20/21 Semester 1, Week 13 </p>
+
+        </div>
+        
+        <div id="clear"></div>
+</div>
+</div>
 </template>
 
 <script>
@@ -9,5 +24,39 @@ export default {
 </script>
 
 <style scoped>
+    #container {
+        width: 1500px;
+        background-color:white;
+        margin: auto;
+    }
+    #first {
+        width: 600px;
+        float: left;
+        height: 100px;
+        align-content: left ;
+    }
+    #second {
+        width: 500px;
+        float: left;
+        height: 100px;
+
+    }
+
+    #personal_message {
+        color: black;
+        font-size: 30px;
+        text-align: left;
+        font-weight: bold;
+        
+    }
+    #school_term {
+        color: black;
+        font-size: 15px;
+        text-align: right;
+        
+    }
+    #clear {
+        clear: both;
+    }
 
 </style>
