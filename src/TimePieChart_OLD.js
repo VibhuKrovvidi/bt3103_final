e93253a9e93d3f6c394403843c23ab4165d6ef55
darreyyll <71427438+darreyyll@users.noreply.github.com>
2020-10-21 23:22:35 +0800

    reverting old timepiechart code.

--- src/TimePieChart_OLD.js
+++ src/TimePieChart_OLD.js
@@ -44,7 +44,7 @@ export default{
     },
 
     methods: {
-        /*fetchItems: function() {
+        fetchItems: function() {
             database.collection('time').get().then(querySnapShot => {
                 querySnapShot.forEach(doc => {
                     this.datacollection.labels.push(doc.data().category)
@@ -52,29 +52,7 @@ export default{
                 })
                 this.renderChart(this.datacollection, this.options)
             })
-        }*/
-        fetchItems: function() {
-            database.collection('time').get().then((querySnapShot) => {
-                querySnapShot.forEach(doc => {
-                    if (doc.data().nusnet == this.iden) {
-                        let curr = doc.data()
-                        let label = curr.category
-                        let idx = this.categories.indexOf(label)
-                        //conversion to minutes
-                        let end = (curr.end%100) + (Math.floor(curr.end/100)*60)
-                        let start = (curr.start%100) + (Math.floor(curr.start/100)*60)
-                        //conversion to hours
-                        let currSum = (((end-start)/60))
-                        if (idx == '-1') {
-                            this.categories.push(curr.category)
-                            this.agg.push(currSum)
-                        } else {
-                            this.agg[idx] += currSum
-                        }
-                    }
-                })
-            })
-        },
+        }
     },
 
     created(){
