e7daf9652d243376e5372eb203cf69525b3e67a5
vibhukrovvidi <vibkrovvidi@gmail.com>
2020-10-22 15:10:32 +0800

    Update AboutUs.vue
    Hover over div to show text

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -2,14 +2,14 @@
 <div class="app">
     <h1> About Us </h1>
     <div id="container">
-        <div id="first">
-            <h3 id='component_header'> Take Control Of Your Time </h3>
-                <p id='time_content'>fkejajkanjkanna</p>
+        <div id="first" @mouseover="hover1 = true" @mouseleave="hover1 = false">
+            <h3 id='component_header' > Take Control Of Your Time </h3>
+                <p id='time_content' v-if="hover1">fkejajkanjkanna</p>
 
         </div>
-        <div id="second">
+        <div id="second" @mouseover="hover2 = true" @mouseleave="hover2 = false">
             <h3 id='component_header'> Information at your fingertips </h3>
-            <p id='nus_content'>ffkejajkanjkanna</p>
+            <p id='nus_content' v-if="hover2">ffkejajkanjkanna</p>
 
         </div>
         
@@ -20,7 +20,13 @@
 
 <script>
 export default {
-    
+    data () {
+        return {
+            hover1 : false,
+            hover2 : false
+        }
+        
+    }
 }
 </script>
 
@@ -40,15 +46,26 @@ export default {
         width: 500px;
         float: left;
         height: 300px;
-            background-color:#526C80;
+        background-color:#526C80;
     }
+    #first:hover {
+        background-color:#003D7C;
+        color:white;
+    }
+    
     #second {
         width: 500px;
         float: left;
         height: 300px;
         background-color: #F5AD69;
+        
     }
 
+    #second:hover{
+        background-color: #EF7C00;
+        color: white;
+    }
+    
     #component_header {
         color: whitesmoke;
         font-size: 30px;
