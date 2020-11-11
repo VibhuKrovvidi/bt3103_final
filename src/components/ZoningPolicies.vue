ffe683288f8885e784d44fc4b39e5e6af453e68d
vibhukrovvidi <vibkrovvidi@gmail.com>
2020-11-11 17:32:01 +0800

    Update ZoningPolicies.vue
    Added form filtering

--- src/components/ZoningPolicies.vue
+++ src/components/ZoningPolicies.vue
@@ -4,7 +4,8 @@
         <h1> Zoning </h1>
         <h3>Zoning Policies</h3>
         <h3> Filter By:
-            <select v-model="selectedoption">
+            <select v-model="selectedoption" @change="get_policies()">
+                <option value="All Policies" > All Policies </option>
                 <option value="Campus Policies">Campus Policies</option>
                 <option value="Visitor Policies">Visitor Policies</option>
                 <option value="Student Life and CCA Activities">Student Life and CCA Activities</option>
@@ -12,18 +13,23 @@
                 <option value="Other Policies">Other Policies</option>
             </select></h3>
         
-        <br><br>
+        
 
-        <table>
-            <tr>
-                <th> Date </th>
-                <td> Title </td>
-            </tr>
-            <tr>
-                <th> Date </th>
-                <td> Title </td>
-            </tr>
-        </table>
+        <div class="notice_cont">
+            
+            <div class="cont"> 
+            <h2 style="text-align:center"> <u>{{selectedoption}} </u> </h2>
+            <ul>
+                    <li v-for="fb in policies" v-bind:key="fb.category" @click="toggleShow">
+                        <div class="category">  <p><u>{{fb.title}} </u> </p>
+                        <div class="details"> <p> <i> {{fb.category}}, posted on {{fb.date}} </i> </p> </div>
+                        </div>
+                        <div class="contents" v-if="showcontents"> <p>{{fb.content}}</p> </div>
+                        
+                    </li>
+                </ul>
+            </div>
+        </div>
 
         <button id=back v-on:click="redirectToZoning()"> Back </button>
         
@@ -31,17 +37,52 @@
 </template>
 
 <script>
+import database from '../firebase.js'
+
 export default {
     data() {
         return {
-            selectedoption: ""
+            selectedoption: "",
+            policies: [],
+            showcontents : ""
         }
     },
 
     methods: {
         redirectToZoning() {
             this.$router.push({path: '/zoning'})
+        },
+        get_policies : function() {
+            this.policies = []
+            database.collection("circulars").get().then((queryDocumentSnapShot) => {           
+                    let fb = {}         
+                    queryDocumentSnapShot.forEach(doc => { 
+                        if(doc.data().category == this.selectedoption) {
+                            fb = doc.data()
+                            console.log(fb)
+                            this.policies.push(fb)
+                        } 
+                        if(this.selectedoption == "All Policies") {
+                            fb = doc.data()
+                            this.policies.push(fb)
+                        }
+                        
+                        
+                    })
+                }
+            )
+        },
+        toggleShow : function() {
+            if(this.showcontents) {
+                this.showcontents = false;
+            } else {
+                this.showcontents = true;
+            }
+
         }
+    },
+    created : function() {
+        this.get_policies()
     }
 
     
@@ -100,6 +141,57 @@ td, th {
 tr:nth-child(even){
     background-color: #ebf1faff;
 }
+
+.notice_cont {
+    width: 100%;
+    height: 580px;
+    
+    position: relative;
+    float: left;
+    float: right;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    
+
+    letter-spacing: 0.75px;
+    margin-left: 0px;
+    text-decoration: none;
+    padding-right: 0px;
+    }   
+
+.cont {
+    position: absolute;
+    height: 300px;
+    width: 720px;
+    top: 0;
+    left: 20%;
+    
+    border: none;
+    
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    cursor: pointer;
+    text-align: left;
+    }
+
+    .category {
+        font-size: 1.25em;
+        text-align: left;
+        cursor: pointer;
+        border:none;
+    }
+
+    .details {
+        color: gray;
+        text-align: left;
+        font-size: smaller;
+       
+
+    }
+
+    .contents {
+        font-size: 1em;
+        text-decoration: none;
+        text-align: left;
+    }
 
 </style>
 
