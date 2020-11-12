ec92d0d527f88aaa11e820a682bfbd3285f755b8
darreyyll <71427438+darreyyll@users.noreply.github.com>
2020-11-12 21:56:43 +0800

    Update DB

--- src/AggregateTime.js
+++ src/AggregateTime.js
@@ -8,7 +8,7 @@ export default {
             datacollection: {
                 labels: ["Work","Social","Exercise","Sleep"],
                 datasets: [{
-                    label: "total number of hours",
+                    label: "Average number of hours",
                     backgroundColor: "navy",
                     data: [0,0,0,0]
                 }]
@@ -27,7 +27,7 @@ export default {
 
                 title: {
                     display: true,
-                    text: "Aggregate Time Spent",
+                    text: "Average Time Spent Across Users",
                     fontSize: 14
                     
                 },
@@ -57,38 +57,72 @@ export default {
     },
 
     methods: {
-        fetchItems: function() {
+        fetchItems: async function() {
+            let fin = new Array()
+            //Compute Average for EACH USER first
+            let getBase = await (
             database.collection("users").get().then((qss1) => {
                 qss1.forEach(doc1 => {
                     database.collection("users").doc(doc1.id).collection("time").get().then((qss2) => {
-                        qss2.forEach(doc2 => {
-                            let cat = doc2.data().category
-                            let curr = doc2.data()
-
-                            //conversion to minutes
-                            let end = (curr.end%100) + (Math.floor(curr.end/100)*60)
-                            let start = (curr.start%100) + (Math.floor(curr.start/100)*60)
-
-                            //conversion to hours
-                            let duration = (((end-start)/60))
-                            console.log(this.datacollection.datasets[0].data)
-
-                            if (cat == "work") {
-                                this.datacollection.datasets[0].data[0] += duration
-                            } else if (cat == "social") {
-                                this.datacollection.datasets[0].data[1] += duration
-                            } else if (cat == "exercise") {
-                                this.datacollection.datasets[0].data[2] += duration
-                            } else if (cat == "sleep") {
-                                this.datacollection.datasets[0].data[3] += duration
-                            }
-                        })
+                        if (!qss2.empty) {
+                            let work = 0
+                            let soc = 0
+                            let exe = 0
+                            let slp = 0
+                            let dates = new Set()
+                            qss2.forEach(doc => {
+                                let curr = doc.data()
+                                //conversion to minutes
+                                let end = (curr.end%100) + (Math.floor(curr.end/100)*60)
+                                let start = (curr.start%100) + (Math.floor(curr.start/100)*60)
+                                //conversion to hours
+                                let currSum = (((end-start)/60))
+                                if (curr.category == "work") {
+                                    this.total++
+                                    dates.add(curr.date)
+                                    work += currSum
+                                } else if (curr.category == "social") {
+                                    this.total++
+                                    dates.add(curr.date)
+                                    soc += currSum
+                                } else if (curr.category == "exercise") {
+                                    this.total++
+                                    dates.add(curr.date)
+                                    exe += currSum
+                                } else if (curr.category == "sleep") {
+                                    this.total++
+                                    dates.add(curr.date)
+                                    slp += currSum
+                                }
+                            })
+                            let denom = dates.size
+                            let res = [work,soc,exe,slp].map(function(x) { return x/denom; })
+                            fin.push(res)
+                        }
+                    })
+                })
+            }))
+            console.log(getBase)
+            let c = 0
+            let getData = await (
+            //Aggregate average across all user's average
+            database.collection("users").get().then((qss1) => {
+                qss1.forEach(doc1 => {
+                    database.collection("users").doc(doc1.id).collection("time").get().then((qss2) => {
+                        if (!qss2.empty) {
+                            this.datacollection.datasets[0].data[0] += fin[c][0]*(1/fin.length)
+                            this.datacollection.datasets[0].data[1] += fin[c][1]*(1/fin.length)
+                            this.datacollection.datasets[0].data[2] += fin[c][2]*(1/fin.length)
+                            this.datacollection.datasets[0].data[3] += fin[c][3]*(1/fin.length)
+                            c++
+                        }
                         this.renderChart(this.datacollection, this.options)
                     })
                 })
-            })
+            }))
+            console.log(getData)
         }
-    },
+    },  
 
     created(){
         this.fetchItems()
